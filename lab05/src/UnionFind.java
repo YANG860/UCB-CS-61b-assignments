437c50f2c05e273ba177e8ac2f72c6b82b810ef8
ShenZhou T8pro <2291779138@qq.com>
2025-05-18 23:28:52 +0800

    5.18 lab05 finished

--- lab05/src/UnionFind.java
+++ lab05/src/UnionFind.java
@@ -1,46 +1,82 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class UnionFind {
     // TODO: Instance variables
 
-    /* Creates a UnionFind data structure holding N items. Initially, all
-       items are in disjoint sets. */
+    List<Integer> arr;
+    int cap;
+
+    /*
+     * Creates a UnionFind data structure holding N items. Initially, all
+     * items are in disjoint sets.
+     */
     public UnionFind(int N) {
-        // TODO: YOUR CODE HERE
+        this.cap = N;
+        this.arr = new ArrayList<>(N);
+        for (int i = 0; i < N; i++) {
+            this.arr.add(-1);
+        }
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        return -1 * this.arr.get(find(v));
     }
 
-    /* Returns the parent of V. If V is the root of a tree, returns the
-       negative size of the tree for which V is the root. */
+    /*
+     * Returns the parent of V. If V is the root of a tree, returns the
+     * negative size of the tree for which V is the root.
+     */
     public int parent(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        return this.arr.get(v);
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO: YOUR CODE HERE
-        return false;
+        return find(v1) == find(v2);
     }
 
-    /* Returns the root of the set V belongs to. Path-compression is employed
-       allowing for fast search-time. If invalid items are passed into this
-       function, throw an IllegalArgumentException. */
+    /*
+     * Returns the root of the set V belongs to. Path-compression is employed
+     * allowing for fast search-time. If invalid items are passed into this
+     * function, throw an IllegalArgumentException.
+     */
     public int find(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+
+        if (v >= this.cap) {
+            throw new IllegalArgumentException();
+        }
+        if (this.arr.get(v) < 0) {
+            return v;
+        } else {
+            this.arr.set(v, find(this.arr.get(v)));
+            return this.arr.get(v);
+        }
     }
 
-    /* Connects two items V1 and V2 together by connecting their respective
-       sets. V1 and V2 can be any element, and a union-by-size heuristic is
-       used. If the sizes of the sets are equal, tie break by connecting V1's
-       root to V2's root. Union-ing an item with itself or items that are
-       already connected should not change the structure. */
+    /*
+     * Connects two items V1 and V2 together by connecting their respective
+     * sets. V1 and V2 can be any element, and a union-by-size heuristic is
+     * used. If the sizes of the sets are equal, tie break by connecting V1's
+     * root to V2's root. Union-ing an item with itself or items that are
+     * already connected should not change the structure.
+     */
     public void union(int v1, int v2) {
-        // TODO: YOUR CODE HERE
+        if (v1 == v2) {
+            return;
+        }
+
+        int newSize = sizeOf(v2) + sizeOf(v1);
+        newSize = -1 * newSize;
+        if (sizeOf(v1) <= sizeOf(v2)) {
+
+            this.arr.set(find(v2), newSize);
+            this.arr.set(find(v1), find(v2));
+        } else {
+            this.arr.set(find(v1), newSize);
+            this.arr.set(find(v2), find(v1));
+        }
     }
 
 }
