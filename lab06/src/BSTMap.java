a5d52d5880501be5fb13d11fc51f03b2fb897d89
ShenZhou T8pro <2291779138@qq.com>
2025-06-05 09:21:16 +0800

    BST Completed

--- lab06/src/BSTMap.java
+++ lab06/src/BSTMap.java
@@ -1,5 +1,7 @@
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
@@ -27,20 +29,59 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
             this.left = new Node();
         }
 
+        private void clear() {
+            this.key = null;
+            this.value = null;
+            this.right = null;
+            this.left = null;
+        }
+
         public boolean isNullDummy() {
             return (this.value == null && this.key == null && this.left == null && this.right == null) ? true : false;
         }
 
-        public Node getCloest(K k) {
+        public Node getInsertNode(K k) {
             if (this.isNullDummy())
                 return this;
             else if (this.key.equals(k))
                 return this;
 
             else if (k.compareTo(this.key) < 0)
-                return this.left.getCloest(k);
+                return this.left.getInsertNode(k);
             else
-                return this.right.getCloest(k);
+                return this.right.getInsertNode(k);
+        }
+
+        public Node getCloestNode(K k) {
+            if (this.isNullDummy())
+                return null;
+
+            else if (this.key.equals(k))
+                return this;
+
+            else if (k.compareTo(this.key) < 0) {
+                // Left
+                if (this.left.isNullDummy())
+                    return this;
+                else
+                    return this.left.getInsertNode(k);
+
+            } else {
+                // Right
+                if (this.right.isNullDummy())
+                    return this;
+                else
+                    return this.right.getInsertNode(k);
+            }
+        }
+
+        public void keySetHelper(Set<K> set) {
+            if (this.isNullDummy())
+                return;
+
+            this.left.keySetHelper(set);
+            set.add(this.key);
+            this.right.keySetHelper(set);
         }
     }
 
@@ -53,7 +94,11 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     }
 
     public void put(K k, V v) {
-        Node ans = root.getCloest(k);
+        if (k == null) {
+            throw new NullPointerException();
+        }
+
+        Node ans = root.getInsertNode(k);
         if (ans.isNullDummy()) {
             this.size++;
             ans.set(k, v);
@@ -66,7 +111,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
             throw new NullPointerException();
         }
 
-        Node ans = root.getCloest(k);
+        Node ans = root.getInsertNode(k);
         if (ans.isNullDummy())
             return null;
         else
@@ -74,7 +119,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     }
 
     public boolean containsKey(K k) {
-        Node ans = root.getCloest(k);
+        Node ans = root.getInsertNode(k);
         return !ans.isNullDummy();
     }
 
@@ -88,15 +133,57 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     }
 
     public V remove(K k) {
-        throw new UnsupportedOperationException();
+
+        // ???
+
+        if (k == null) {
+            throw new NullPointerException();
+        }
+
+        Node x = this.root.getInsertNode(k);
+        if (x == null || x.key != k)
+            return null;
+
+        else {
+            this.size--;
+            V ans = x.value;
+
+            if (!x.left.isNullDummy()) {
+                Node closest = x.left.getCloestNode(k);
+                x.key = closest.key;
+                x.value = closest.value;
+
+                closest.key = closest.left.key;
+                closest.value = closest.left.value;
+                closest.right = closest.left.right;
+                closest.left = closest.left.left;
+
+            } else if (!x.right.isNullDummy()) {
+                Node closest = x.right.getCloestNode(k);
+                x.key = closest.key;
+                x.value = closest.value;
+
+                closest.key = closest.right.key;
+                closest.value = closest.right.value;
+                closest.left = closest.right.left;
+                closest.right = closest.right.right;
+
+            } else {
+                x.clear();
+            }
+            return ans;
+        }
+
     }
 
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> ans = new TreeSet<>();
+        this.root.keySetHelper(ans);
+        return ans;
     }
 
-    public Iterator iterator() {
-        throw new UnsupportedOperationException();
+    public Iterator<K> iterator() {
+        return this.keySet().iterator();
     }
 
 }
